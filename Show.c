0218318671f87bc400e9b8f2b91680d8ec562f26
Maksim Kunts <kuncz.maks@mail.ru>
2026-09-20 15:28:49 +0300

    Write simple file viewer

--- Show.c
+++ Show.c
@@ -18,6 +18,16 @@ void terminate_curses(void) {
   endwin();
 }
 
+char *read_line(char *buf, size_t bufsize, FILE *fin) {
+  if (!fgets(buf, bufsize, fin)) {
+    return NULL;
+  }
+  size_t cur_len = strlen(buf);
+  if (buf[cur_len - 1] == '\n') {
+    buf[cur_len - 1] = '\0';
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: ./Show <path/to/file>\n");
@@ -33,24 +43,41 @@ int main(int argc, char *argv[]) {
 
   init_curses();
   {
+    size_t linebuf_size = COLS;
+    char *linebuf = malloc(linebuf_size);
     WINDOW *frame = newwin(LINES, COLS, 0, 0);
     WINDOW *win = newwin(LINES - 2, COLS - 2, 1, 1);
 
     box(frame, 0, 0);
-    mvwaddstr(frame, 0, 4, "[");
-    mvwaddstr(frame, 0, 5, path);
-    mvwaddstr(frame, 0, 5+strlen(path), "]");
+    const size_t PATH_OUTPUT_OFFSET = 4;
+    mvwaddstr(frame, 0, PATH_OUTPUT_OFFSET, "[");
+    mvwaddstr(frame, 0, PATH_OUTPUT_OFFSET+1, path);
+    mvwaddstr(frame, 0, PATH_OUTPUT_OFFSET+1+strlen(path), "]");
     wrefresh(frame);
     keypad(win, TRUE);
     scrollok(win, TRUE);
 
+    for (int i = 0; i < LINES-2; ++i) {
+      if (!read_line(linebuf, linebuf_size, fin)) {
+        break;
+      }
+      wprintw(win, "\n%s", linebuf);
+    }
+
     int c = 0;
     while ((c = wgetch(win)) != 27) {
-      wprintw(win, "\n%d: %s", c, keyname(c));
+      if (c == ' ') {
+        if (!read_line(linebuf, linebuf_size, fin)) {
+          wprintw(win, "\n~");
+        } else {
+          wprintw(win, "\n%s", linebuf);
+        }
+      }
     }
 
     delwin(win);
     delwin(frame);
+    free(linebuf);
   }
   terminate_curses();
 
