82d0268467f8e464ad10973589c1d990b16211b5
Maksim Kunts <kuncz.maks@mail.ru>
2026-09-20 14:52:00 +0300

    Print path

--- Show.c
+++ Show.c
@@ -1,32 +1,59 @@
-#include <curses.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include <locale.h>
+#include <string.h>
+#include <errno.h>
 
-#define DX 7
-#define DY 3
+#include <curses.h>
 
-int main(int argc, char *argv[]) {
-  WINDOW *frame, *win;
-  int c = 0;
-
+void init_curses(void) {
   setlocale(LC_ALL, "");
   initscr();
   noecho();
   cbreak();
-  printw("Window:");
-  refresh();
+}
 
-  frame = newwin(LINES - 2 * DY, COLS - 2 * DX, DY, DX);
-  box(frame, 0, 0);
-  mvwaddstr(frame, 0, (int)((COLS - 2 * DX - 5) / 2), "Frame");
-  wrefresh(frame);
-
-  win = newwin(LINES - 2 * DY - 2, COLS - 2 * DX - 2, DY + 1, DX + 1);
-  keypad(win, TRUE);
-  scrollok(win, TRUE);
-  while ((c = wgetch(win)) != 27)
-    wprintw(win, "\n%d: %s", c, keyname(c));
-  delwin(win);
-  delwin(frame);
+void terminate_curses(void) {
   endwin();
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: ./Show <path/to/file>\n");
+    return -1;
+  }
+
+  const char *path = argv[1];
+  FILE *fin = fopen(path, "r");
+  if (!fin) {
+    perror(path);
+    return errno;
+  }
+
+  init_curses();
+  {
+    WINDOW *frame = newwin(LINES, COLS, 0, 0);
+    WINDOW *win = newwin(LINES - 2, COLS - 2, 1, 1);
+
+    box(frame, 0, 0);
+    mvwaddstr(frame, 0, 4, "[");
+    mvwaddstr(frame, 0, 5, path);
+    mvwaddstr(frame, 0, 5+strlen(path), "]");
+    wrefresh(frame);
+    keypad(win, TRUE);
+    scrollok(win, TRUE);
+
+    int c = 0;
+    while ((c = wgetch(win)) != 27) {
+      wprintw(win, "\n%d: %s", c, keyname(c));
+    }
+
+    delwin(win);
+    delwin(frame);
+  }
+  terminate_curses();
+
+  fclose(fin);
   return 0;
 }
