04fe936a789beaf5e4ceb9f5dfc6d6c968e2c082
Maksim Kunts <kuncz.maks@mail.ru>
2026-09-20 14:05:35 +0300

    Frame example

--- Show.c
+++ Show.c
@@ -1,10 +1,32 @@
 #include <curses.h>
+#include <locale.h>
 
-int main(void)
-{
-  initscr ();
-  move (LINES/2 - 1, COLS/2 - 4);
-  addstr ("Hello, World!");
-  getch ();
-  endwin ();
+#define DX 7
+#define DY 3
+
+int main(int argc, char *argv[]) {
+  WINDOW *frame, *win;
+  int c = 0;
+
+  setlocale(LC_ALL, "");
+  initscr();
+  noecho();
+  cbreak();
+  printw("Window:");
+  refresh();
+
+  frame = newwin(LINES - 2 * DY, COLS - 2 * DX, DY, DX);
+  box(frame, 0, 0);
+  mvwaddstr(frame, 0, (int)((COLS - 2 * DX - 5) / 2), "Frame");
+  wrefresh(frame);
+
+  win = newwin(LINES - 2 * DY - 2, COLS - 2 * DX - 2, DY + 1, DX + 1);
+  keypad(win, TRUE);
+  scrollok(win, TRUE);
+  while ((c = wgetch(win)) != 27)
+    wprintw(win, "\n%d: %s", c, keyname(c));
+  delwin(win);
+  delwin(frame);
+  endwin();
+  return 0;
 }
